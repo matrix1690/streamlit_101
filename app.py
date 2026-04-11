56b435e032c3f00b3a54da75c67a9ab3f1d2d92d
zioasso <zioassojoe@gmail.com>
2026-04-11 18:59:39 +0000

    Correction Black

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-df = pd.DataFrame({
-    'a': [1, 2, 3, 4],
-    'b': [10, 20, 30, 40],
-})
+df = pd.DataFrame(
+    {
+        "a": [1, 2, 3, 4],
+        "b": [10, 20, 30, 40],
+    }
+)
 
 with st.sidebar:
     st.write("Hello World Gaye!!!")
 
     option = st.selectbox(
         "Selectionnez une formation à suivre",
-        ["Joins", "group By","Windows Functions"],
-        index = None,
+        ["Joins", "group By", "Windows Functions"],
+        index=None,
         placeholder="Choisir une formation...",
         # accept_new_options=True,
     )
 
-st.write("Vous avez choissi : ", option )
-sql_query = st.text_area('Veuillez saisir la requête')
+st.write("Vous avez choissi : ", option)
+sql_query = st.text_area("Veuillez saisir la requête")
 
 if sql_query != "":
     result = duckdb.query(str(sql_query)).df()
@@ -27,8 +29,4 @@ else:
     result = pd.DataFrame({})
 
 
-
-
 st.write(result)
-
-
